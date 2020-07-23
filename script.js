6f57a2e406b8cecfe475fe0d525e987663c4bc70
Hannah Stover <hrosestover@gmail.com>
2020-07-23 09:40:14 -0700

    updated scroll-to-top speed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var link = document.getElementById('back-to-top');
 var amountScrolled = 250;
 
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', function (e) {
   if (window.pageYOffset > amountScrolled) {
     link.classList.add('show');
   } else {
@@ -10,17 +10,17 @@ window.addEventListener('scroll', function(e) {
 });
 
 // Scrolls to Top
-link.addEventListener('click', function(e) {
-  e.preventDefault();
+// link.addEventListener('click', function (e) {
+//   e.preventDefault();
 
-  var distance = 0 - window.pageYOffset;
-  var increments = distance / (500 / 16);
-  function animateScroll() {
-    window.scrollBy(0, increments);
-    if (window.pageYOffset <= document.body.offsetTop) {
-      clearInterval(runAnimation);
-    }
-  }
-  // Loop the animation function
-  var runAnimation = setInterval(animateScroll, 16);
-});
+//   var distance = 0 - window.pageYOffset;
+//   var increments = distance / (500 / 16);
+//   function animateScroll() {
+//     window.scrollBy(0, increments);
+//     if (window.pageYOffset <= document.body.offsetTop) {
+//       clearInterval(runAnimation);
+//     }
+//   }
+//   // Loop the animation function
+//   var runAnimation = setInterval(animateScroll, 16);
+// });
